dc4e8af1dad00dbf18b51c519dc4b68f55527034
Spark Hao <email@example.com>
2022-06-06 09:45:42 +0800

    update all

--- server.mjs
+++ server.mjs
@@ -1,4 +1,4 @@
-import { promisify } from 'util' 
+import { promisify } from 'util'
 import { exec } from 'child_process'
 import fs from 'fs'
 
@@ -15,56 +15,156 @@ const fastify = Fastify({
 fastify.register(fastifyCors)
 
 fastify.post('/compile', async (request, reply) => {
-  // console.log('Request body', request.body)
-  if (!request.body || !request.body['lib.rs']) {
-    return (
-      reply
-        .code(400)
-        .header('Content-Type', 'application/json; charset=utf-8')
-        .send({
-          error: 'Missing data for lib.rs'
-        })
-    )
-  }
-
-  fs.writeFileSync('fil-hello-world-actor/src/lib.rs', request.body['lib.rs'])
-  if (request.body['Cargo.toml']) {
-    fs.writeFileSync('fil-hello-world-actor/Cargo.toml', request.body['Cargo.toml'])
-  }
-
-  try {
-    const { stdout, stderr } = await execWithPromise(
-      'cargo build',
-      {
-        cwd: 'fil-hello-world-actor'
+  const action = request.body['action']
+  if ( action ) {
+    switch ( action ) {
+      case 1:  {
+        const {stdout, stderr} = await execWithPromise(
+            './lotus chain install-actor ../web/fil-hello-world-actor/target/debug/wbuild/fil_hello_world_actor/fil_hello_world_actor.compact.wasm',
+            {
+              cwd: '../lotus'
+            }
+        )
+        return {
+          success: true,
+          logs: stdout,
+        }
       }
-    )
-    const buffer = fs.readFileSync(
-      'fil-hello-world-actor/target/debug/wbuild/' +
-      'fil_hello_world_actor/fil_hello_world_actor.compact.wasm'
-    )
-    const encoded = encode([ buffer ])
-    const base64Encoded = encoded.toString('base64')
-    return {
-      success: true,
-      logs: stderr,
-      wasmBinaryParamsEncoded: base64Encoded
+      case 2:  {
+        const cid = request.body['cid']
+        const cmd_create = './lotus chain create-actor ' + cid
+        const {stdout, stderr} = await execWithPromise(
+            cmd_create,
+            {
+              cwd: '../lotus'
+            }
+
+        )
+        return {
+          success: true,
+          logs: stdout,
+        }
+      }
+
+      case 3:  {
+        const actorId = request.body['actorId']
+        const method = request.body['method']
+        const params = request.body['params']
+        const cmd_invoke = './lotus chain invoke ' + actorId + " " + method + " " + params
+        const {stdout, stderr} = await execWithPromise(
+            cmd_invoke.trim(),
+            {
+              cwd: '../lotus'
+            }
+
+        )
+        return {
+          success: true,
+          logs: stdout,
+        }
+      }
+
+      case 4:  {
+        const actorId = request.body['actorId']
+        const method = request.body['method']
+        const params = request.body['params']
+        const cmd_send = './lotus send ' + actorId + " " + params
+        const {stdout, stderr} = await execWithPromise(
+            cmd_send.trim(),
+            {
+                cwd: '../lotus'
+            }
+
+        )
+        return {
+            success: true,
+            logs: stdout,
+        }
+      }
+
+      case 5:  {
+            const actorId = request.body['actorId']
+            const method = request.body['method']
+            const params = request.body['params']
+            const value = request.body['value']
+            const cmd_invoke2 = './lotus chain invoke2 ' + actorId + " " + method + " " + params +" "+ value
+            const {stdout, stderr} = await execWithPromise(
+                cmd_invoke2.trim(),
+                {
+                    cwd: '../lotus'
+                }
+
+            )
+            return {
+                success: true,
+                logs: stdout,
+            }
+      }
+
+      default: return (
+          reply
+              .code(400)
+              .header('Content-Type', 'application/json; charset=utf-8')
+              .send({
+                success: false,
+                error: "action not supported"
+              })
+      )
     }
-  } catch (e) {
-    console.error('Exception:', e.message)
-    console.error('Code:', e.code)
-    console.error('stdout:\n', e.stdout)
-    console.error('stderr:\n', e.stderr)
-    return (
-      reply
-        .code(400)
-        .header('Content-Type', 'application/json; charset=utf-8')
-        .send({
-          success: false,
-          code: e.code,
-          error: e.stderr
-        })
-    )
+  }else {
+    if (!request.body || !request.body['lib.rs']) {
+      return (
+          reply
+              .code(400)
+              .header('Content-Type', 'application/json; charset=utf-8')
+              .send({
+                error: 'Missing data for lib.rs'
+              })
+      )
+    }
+    // fs.writeFileSync('fil-hello-world-actor/src/lib.rs', request.body['lib.rs'])
+    // if (request.body['Cargo.toml']) {
+    //   fs.writeFileSync('fil-hello-world-actor/Cargo.toml', request.body['Cargo.toml'])
+    // }
+
+    try {
+      const { stdout, stderr } = await execWithPromise(
+          'cargo build',
+          {
+            cwd: 'fil-hello-world-actor'
+          }
+      )
+      const buffer = fs.readFileSync(
+          'fil-hello-world-actor/target/debug/wbuild/' +
+          'fil_hello_world_actor/fil_hello_world_actor.compact.wasm'
+      )
+      const encoded = encode([ buffer ])
+      const base64Encoded = encoded.toString('base64')
+      const result_data = stderr
+      return {
+        success: true,
+        logs: result_data,
+        wasmBinaryParamsEncoded: base64Encoded
+      }
+    } catch (e) {
+      console.log("-----------", e)
+      console.error('Exception:', e.message)
+      console.error('Code:', e.code)
+      console.error('stdout:\n', e.stdout)
+      console.error('stderr:\n', e.stderr)
+      return (
+          reply
+              .code(400)
+              .header('Content-Type', 'application/json; charset=utf-8')
+              .send({
+                success: false,
+                code: e.code,
+                error: e.stderr,
+                msg: e.message,
+                out: e.stdout,
+              })
+      )
+    }
   }
 })
 
--- test-curl-lotus-api-production.sh
+++ test-curl-lotus-api-production.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-#URL=http://127.0.0.1:3000
-URL=https://fvm-2.default.knative.hex.camp
+URL=http://127.0.0.1:3000
+#URL=https://fvm-2.default.knative.hex.camp
 
 set -x
 curl -v -X POST   -H "Content-Type: application/json"   --data '{ 
